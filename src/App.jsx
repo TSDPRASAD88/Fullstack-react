fa8d5a27c290929c4b021025836abe7359191781
TSDPRASAD88 <prasad.thamarana2006@gmail.com>
2025-03-28 13:29:41 +0530

    REACT

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>NAME:T SATYA DURGA PRASAD</h1>
+        <h2>KALVIUM ID:thamarana.sdurgaprasad.s89@kalvium.community</h2>
+        <h2>GIT-HUB:TSDPRASAD88</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
